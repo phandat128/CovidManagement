d45294aa4a49ac6beb30460d820a72109d18b361
haolvtt <levanhao123321@gmail.com>
2022-12-23 11:56:29 +0700

    Update NhanKhauModel.java

--- src/main/java/covidmanagement/model/NhanKhauModel.java
+++ src/main/java/covidmanagement/model/NhanKhauModel.java
@@ -1,4 +1,94 @@
 package covidmanagement.model;
 
+import java.util.Date;
+
 public class NhanKhauModel {
+    private String MaNhanKhau;
+    private String HoTen;
+    private Date NgaySinh;
+    private String GioiTinh;
+    private String CMNDCCCD;
+    private String SDT;
+    private String QuocTich;
+    private String TonGiao;
+    private String NgheNghiep;
+    private String MaHoKhau;
+    private String ChuHo;
+    private String QuanHeVoiChuHo;
+
+
+    public String getMaNhanKhau() {
+        return MaNhanKhau;
+    }
+    public void setMaNhanKhau(String MaNhanKhau) {
+        this.MaNhanKhau = MaNhanKhau;
+    }
+    public String getHoTen(){
+        return HoTen;
+    }
+    public void setHoTen(String HoTen) {
+        this.HoTen = HoTen;
+    }
+    public Date getNgaySinh() {
+        return NgaySinh;
+    }
+    public void setNamSinh(Date namSinh) {
+        this.NgaySinh = namSinh;
+    }
+    public String getGioiTinh() {
+        return GioiTinh;
+    }
+    public void setGioiTinh(String gioiTinh) {
+        this.GioiTinh = gioiTinh;
+    }
+    public String getCMNDCCCD() {
+        return CMNDCCCD;
+    }
+
+    public void setCMNDCCCD(String CMNDCCCD) {
+        this.CMNDCCCD = CMNDCCCD;
+    }
+
+    public String getSDT(){
+        return SDT;
+    }
+    public void setSDT(String SDT){
+        this.SDT = SDT;
+    }
+    public String getQuocTich() {
+        return QuocTich;
+    }
+    public void setQuocTich(String quocTich) {
+        this.QuocTich = quocTich;
+    }
+    public String getTonGiao() {
+        return TonGiao;
+    }
+    public void setTonGiao(String tonGiao) {
+        this.TonGiao = tonGiao;
+    }
+    public String getNgheNghiep() {
+        return NgheNghiep;
+    }
+    public void setNgheNghiep(String ngheNghiep) {
+        this.NgheNghiep = ngheNghiep;
+    }
+    public String getMaHoKhau() {
+        return MaHoKhau;
+    }
+    public void setMaHoKhau(String MaHoKhau) {
+        this.MaHoKhau = MaHoKhau;
+    }
+    public String getChuHo() {
+        return ChuHo;
+    }
+    public void setChuHo(String ChuHo) {
+        this.ChuHo = ChuHo;
+    }
+    public String getQuanHeVoiChuHo() {
+        return QuanHeVoiChuHo;
+    }
+    public void setQuanHeVoiChuHo(String QuanHeVoiChuHo) {
+        this.QuanHeVoiChuHo = QuanHeVoiChuHo;
+    }
 }
